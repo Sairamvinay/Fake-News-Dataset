98f3a69b5c3edbe9fbe4b36d756f88e2e77e6012
Armand Nasseri <aanasseri@ucdavis.edu>
2019-11-09 15:14:20 -0800

    spacy prototyping

--- FileParsing.py
+++ FileParsing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import spacy
+spacy_nlp = spacy.load('en_core_web_sm')
 TRAINFILEPATH = "fake-news/train.csv"
 TESTFILEPATH = "fake-news/test.csv"
 
@@ -15,10 +16,22 @@ def read_files(PATH,nolabel = False):
 	df = pd.read_csv(PATH,sep = ",",names= names,header = 0)
 	return df
 
+spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
 
-def main():
-	dfTrain = read_files(TRAINFILEPATH,nolabel = False)
-	dfTest = read_files(TESTFILEPATH,nolabel = True)
+#print('Number of stop words: %d' % len(spacy_stopwords))
+#print('First ten stop words: %s' % list(spacy_stopwords)[:10])
 
-if __name__ == '__main__':
-	main()
+dfTrain = read_files(TRAINFILEPATH,nolabel = False)
+dfTest = read_files(TESTFILEPATH,nolabel = True)
+textTrain = dfTrain["text"]
+
+doc = spacy_nlp(textTrain[0])
+tokens = [token.text for token in doc if not token.is_stop]
+print('Original Article: %s' % (doc))
+print()
+print(tokens)
+
+#print(textTrain[0])
+#print(dfTrain)
+#print(dfTest)
+
