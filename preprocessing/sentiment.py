6fb37dd6bfd0532f9f0a56457106d006934af444
daniel <danielguo123456@hotmail.com>
2019-11-09 16:47:55 -0800

    sentiment:

--- preprocessing/sentiment.py
+++ preprocessing/sentiment.py
@@ -7,19 +7,33 @@ TRAINFILEPATH = "../fake-news/train.csv"
 TESTFILEPATH = "../fake-news/test.csv"
 
 
+def plt_polarity(dat, category):
+    dat.hist(grid=True, bins=50, rwidth=0.9, color='#607c8e')
 
-
-
+    if category == "fake":
+        plt.title('Distribution of Polarity of Fake News Sentiment')
+    elif category == "real":
+        plt.title('Distribution of Polarity of Real News Sentiment')
+    else:  # all news
+        plt.title('Distribution of Polarity of Sentiment')
+    
+    plt.ylabel('Count')
+    plt.xlabel('Polarity')
+    plt.grid(axis='y', alpha=0.75)
 
 
 def main():
-    data = FileProcess.read_files(TRAINFILEPATH,nolabel = False, sample=500)
+    data = FileProcess.read_files(TRAINFILEPATH,nolabel = False)
     data['polarity'] = data['text'].map(lambda x: TextBlob(x).sentiment.polarity)
-    data.hist(column="polarity", bins=30)
-    plt.ylabel('count')
-    plt.xlabel('polarity')
+    real = data.loc[data['label'] == 0] # select rows that are real news
+    fake = data.loc[data['label'] == 1] # select rows that are real news
+    plt.figure(1)
+    plt_polarity(data['polarity'], "all")
+    plt.figure(2)
+    plt_polarity(fake['polarity'], "fake")
+    plt.figure(3)
+    plt_polarity(real['polarity'], "real")
     plt.show()
-
 
 if __name__ == "__main__":
     main()
