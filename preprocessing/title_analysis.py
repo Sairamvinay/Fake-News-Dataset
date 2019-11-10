33abd737ae0155e84a10b15f97e3c1d3c6a7bdd9
xu1998hz <wedxu@ucdavis.edu>
2019-11-09 20:15:32 -0800

    change place

---
+++ preprocessing/title_analysis.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import spacy
+import csv
+import time
+from collections import defaultdict
+nlp = spacy.load('en_core_web_sm')
+
+# load in my data as data frame
+df = pd.read_csv('../fake-news/train.csv')
+
+dfTrue = df[df['label']==0]
+dfFalse = df[df['label']==1]
+
+# find the size of the data frame
+
+i = 1
+samples_features = []
+# collect 5 features for now to do the analysis
+POS = defaultdict(int)
+TAG = defaultdict(int)
+DEP = defaultdict(int)
+ALPHA = defaultdict(int)
+STOP = defaultdict(int)
+with open("FalseDataFeature_dict.csv", "w", newline="") as f:
+    writer = csv.writer(f, delimiter=',')
+    for sen in dfFalse['title'].dropna():
+        start_time = time.time()
+        print(sen)
+        doc = nlp(sen)
+        for token in doc:
+            POS[token.pos_]+=1
+            TAG[token.tag_]+=1
+            DEP[token.dep_]+=1
+            ALPHA[token.is_alpha]+=1
+            STOP[token.is_stop]+=1
+        print(row)
+        samples_features.append(row)
+        second_time = time.time()
+        print("time estimates to load ", i)
+        print("th sample: %s seconds" % (second_time-start_time))
+        i+=1
+row = [POS, TAG, DEP, ALPHA, STOP]
+writer.writerows(row)
