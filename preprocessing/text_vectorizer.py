93ff7bb9daad23f0b900878d31f8d27fa53f6098
Sairamvinay <sairam16898@gmail.com>
2019-11-15 23:23:15 -0800

    Added new main and all

--- preprocessing/text_vectorizer.py
+++ preprocessing/text_vectorizer.py
@@ -1,8 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import gensim
-from gensim.models import Word2Vec
 
+from fileprocess import read_files,TRAINFILEPATH,TESTFILEPATH
 # '''
 # Parameters: 2 arrays of strings which contain the text of each article within the dataset for train and test
 # Returns: 3 arrays: the feature vector for train, the feature vector for test, words (the features fitted on train)
@@ -40,9 +40,41 @@ def TFIDF(training_text,testing_text):
 
 
 def word2vec(training_text, testing_text):
-    model = Word2Vec.load_word2vec_format(
+    modelTrain = gensim.models.KeyedVectors.load_word2vec_format(
             "../fake-news/train_word2vec_model.bin", binary=True)
-    X_train = [model[word] for word in training_text]
-    X_test = [model[word] for word in testing_text]
+
+    modelTest = gensim.models.KeyedVectors.load_word2vec_format("../fake-news/test_word2vec_model.bin",binary = True)
+    X_train = [modelTrain[word] for word in training_text]
+    X_test = [modelTest[word] for word in testing_text]
 
     return X_train, X_test
+
+
+def main():
+    dfTrain = read_files(TRAINFILEPATH,nolabel = False)
+    dfTest = read_files(TESTFILEPATH,nolabel = True)
+    lines_length = len(dfTrain.values)
+    lines_testlength = len(dfTest.values)
+    training_text = [dfTrain["text"].values[i] for i in range(lines_length)]
+    testing_text = [dfTest["text"].values[i] for i in range(lines_testlength)]
+    X_train_TFIDF,X_test_TFIDF,_ = TFIDF(training_text,testing_text)
+    X_train_CV,X_test_CV,_ = CV(training_text,testing_text)
+   
+    print(X_train_TFIDF.shape," is the X_train shape")
+    print(X_test_TFIDF.shape," is the X_test shape")
+
+    print(X_train_CV.shape," is the X_train shape")
+    print(X_test_CV.shape," is the X_test shape")
+
+
+
+if __name__ == '__main__':
+
+    main()
+
+
+
+
+
+
+
