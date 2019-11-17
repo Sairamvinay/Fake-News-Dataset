b6b4603e3b37cb40a9090fc7f77d967c6f245986
Sairamvinay <sairam16898@gmail.com>
2019-11-16 16:29:20 -0800

    Updated text vectorizer

--- preprocessing/text_vectorizer.py
+++ preprocessing/text_vectorizer.py
@@ -1,8 +1,9 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import gensim
-
 from fileprocess import read_files,TRAINFILEPATH,TESTFILEPATH
+from time import time
+import numpy as np
 # '''
 # Parameters: 2 arrays of strings which contain the text of each article within the dataset for train and test
 # Returns: 3 arrays: the feature vector for train, the feature vector for test, words (the features fitted on train)
@@ -38,19 +39,38 @@ def TFIDF(training_text,testing_text):
     return X_train,X_test,words
 
 
+def getVector(model,tokens,size = 100):
+    vec = np.zeros(size)
+    count = 0
+    for word in tokens:
+        try:
+
+            vec += model[word]
+            count += 1.0
+
+        except KeyError:
+            continue
+
+    if count != 0:
+        vec = vec / count
+
+    return vec
+
 
 def word2vec(training_text, testing_text):
-    modelTrain = gensim.models.KeyedVectors.load_word2vec_format(
-            "../fake-news/train_word2vec_model.bin", binary=True)
+    modelTrain = gensim.models.KeyedVectors.load(
+            "../fake-news/train_word2vec_model.bin")
 
-    modelTest = gensim.models.KeyedVectors.load_word2vec_format("../fake-news/test_word2vec_model.bin",binary = True)
-    X_train = [modelTrain[word] for word in training_text]
-    X_test = [modelTest[word] for word in testing_text]
-
-    return X_train, X_test
+    modelTest = gensim.models.KeyedVectors.load("../fake-news/test_word2vec_model.bin")
+    
+    X_train = [getVector(modelTrain,sent.split(' ')) for sent in training_text]
+    X_test =  [getVector(modelTest,sent.split(' ')) for sent in testing_text]
+    return np.array(X_train), np.array(X_test)
 
 
 def main():
+
+    start = time()
     dfTrain = read_files(TRAINFILEPATH,nolabel = False)
     dfTest = read_files(TESTFILEPATH,nolabel = True)
 
@@ -58,20 +78,29 @@ def main():
     
     lines_length = len(dfTrain.values)
     lines_testlength = len(dfTest.values)
+    
     trainVal = dfTrain["text"].values
     testVal = dfTest["text"].values
+    
     training_text = [trainVal[i] for i in range(lines_length)]
     testing_text = [testVal[i] for i in range(lines_testlength)]
+    
     X_train_TFIDF,X_test_TFIDF,_ = TFIDF(training_text,testing_text)
     X_train_CV,X_test_CV,_ = CV(training_text,testing_text)
-    
-    
+    X_train_WV,X_test_WV = word2vec(training_text,testing_text)
+
+    print(X_train_WV.shape, " is the X_train shape")
+    print(X_test_WV.shape, " is the X_test shape")
+
     print(X_train_TFIDF.shape," is the X_train shape")
     print(X_test_TFIDF.shape," is the X_test shape")
 
     print(X_train_CV.shape," is the X_train shape")
     print(X_test_CV.shape," is the X_test shape")
 
+    end = time()
+    taken = (end - start) / 60.00
+    print("Time taken :%f minutes"%taken)
 
 
 if __name__ == '__main__':
