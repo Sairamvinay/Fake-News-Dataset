eb4327171ccf0e68bd58b5b942d1b0cfb3d0319f
daniel <danielguo123456@hotmail.com>
2019-11-17 00:47:28 -0800

    random forest

--- train/random_forest.py
+++ train/random_forest.py
@@ -1,11 +1,11 @@
 import readdata
-from sklearn.ensemble import RandomForestClassifier
 from text_vectorizer import CV
 from text_vectorizer import TFIDF
 from text_vectorizer import word2vec
 from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 import numpy as np
 import sys
 
@@ -31,30 +31,39 @@ def main():
     dfTrain = readdata.read_clean_data(readdata.TRAINFILEPATH,nolabel = False)
     dfTest = readdata.read_clean_data(readdata.TESTFILEPATH,nolabel = True)
 
-    X = dfTrain['text'].to_numpy()
-    Y = dfTrain['label'].to_numpy()
+    X_train = dfTrain['text'].to_numpy()
+    Y_train = dfTrain['label'].to_numpy()
     X_test = dfTest['text'].to_numpy() # unlabelled
 
 
     if sys.argv[1] == "cv":
-        X, X_test, _ = CV(X, X_test) # train shape: (17973, 141221)
+        X_train, X_test, _ = CV(X_train, X_test) # train shape: (17973, 141221)
     elif sys.argv[1] == 'tfidf':
-        X, X_test, _ = TFIDF(X, X_test) # shape: (17973, 141221)
+        X_train, X_test, _ = TFIDF(X_train, X_test) # shape: (17973, 141221)
     elif sys.argv[1] == 'word2vec':
-        X, X_test = word2vec(X, X_test)
+        X_train, X_test = word2vec(X_train, X_test)
     else:
         print("Error")
         return
 
-    kf = KFold(n_splits=3)
-    for train_index, dev_index in kf.split(X):
-        X_train, X_dev = X[train_index], X[dev_index]
-        y_train, y_dev = Y[train_index], Y[dev_index]   
-        model = RandomForestClassifier(n_estimators=20, random_state=0, n_jobs=-1)
-        model.fit(X_train, y_train)
-        print('fit done')
-        y_pred = model.predict(X_dev)
-        evaluate(y_pred, y_dev)
+
+    # below are the hyperparameters to be grid-searched on
+    n_estimators = [200, 400, 800]
+    max_depth = [1, 5, 9]
+    min_samples_leaf = [2, 4]
+    min_samples_split = [5, 10]
+    param_grid = dict(n_estimators=n_estimators, max_depth=max_depth,
+                    min_samples_leaf=min_samples_leaf,
+                    min_samples_split=min_samples_split)
+
+    model = RandomForestClassifier()
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+    grid_result = grid.fit(X_train, Y_train)
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
 
 
 
