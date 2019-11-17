0af29badd22451d6cb4f60093f4f93dc5b454097
daniel <danielguo123456@hotmail.com>
2019-11-16 16:53:02 -0800

    cross-validation

--- train/random_forest.py
+++ train/random_forest.py
@@ -5,6 +5,7 @@ from text_vectorizer import TFIDF
 from text_vectorizer import word2vec
 from sklearn import metrics
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import KFold
 import numpy as np
 import sys
 
@@ -26,28 +27,32 @@ def main():
     dfTrain = readdata.read_clean_data(readdata.TRAINFILEPATH,nolabel = False)
     dfTest = readdata.read_clean_data(readdata.TESTFILEPATH,nolabel = True)
 
-    X_train = dfTrain['text'].to_numpy()
-    Y_train = dfTrain['label'].to_numpy()
-    X_test = dfTest['text'].to_numpy()
+    X = dfTrain['text'].to_numpy()
+    Y = dfTrain['label'].to_numpy()
+    X_test = dfTest['text'].to_numpy() # unlabelled
 
 
     if sys.argv[1] == "cv":
-        X_train, X_test, _ = CV(X_train, X_test) # train shape: (17973, 141221)
+        X, X_test, _ = CV(X, X_test) # train shape: (17973, 141221)
     elif sys.argv[1] == 'tfidf':
-        X_train, X_test, _ = TFIDF(X_train, X_test) # shape: (17973, 141221)
+        X, X_test, _ = TFIDF(X, X_test) # shape: (17973, 141221)
     elif sys.argv[1] == 'word2vec':
-        X_train, X_test = word2vec(X_train, X_test)
+        X, X_test = word2vec(X, X_test)
         MAX_LENGTH = 250
-        X_train = pad_sequences(X_train, maxlen=MAX_LENGTH)
+        X = pad_sequences(X, maxlen=MAX_LENGTH)
         X_test = pad_sequences(X_test, maxlen=MAX_LENGTH)
     else:
         print("Error")
         return
 
-    model = RandomForestClassifier(n_estimators=20, random_state=0, n_jobs=-1)
-    model.fit(X_train, Y_train)
-    y_pred = model.predict(X_train)
-    evaluate(y_pred, Y_train)
+    kf = KFold(n_splits=5)
+    for train_index, dev_index in kf.split(X_train):
+        X_train, X_dev = X[train_index], X[dev_index]
+        y_train, y_dev = Y[train_index], Y[dev_index]   
+        model = RandomForestClassifier(n_estimators=20, random_state=0, n_jobs=-1)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_dev)
+        evaluate(y_pred, y_dev)
 
 
 
