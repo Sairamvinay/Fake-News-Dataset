b9efeeef4bac665985463f36845b29069fa16b03
Sairamvinay <sairam16898@gmail.com>
2019-11-17 17:33:45 -0800

    Added bst statement

--- train/ann.py
+++ train/ann.py
@@ -101,6 +101,7 @@ def main():
 	grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
 
 	grid_result = grid.fit(X_train, Y_train)
+	print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
 	means = grid_result.cv_results_['mean_test_score']
 	stds = grid_result.cv_results_['std_test_score']
 	params = grid_result.cv_results_['params']
