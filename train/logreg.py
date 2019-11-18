4eaad38d428fd6c5ffe9bd2c24aca3cc054cf4f0
jhvoong <jhvoong@ucdavis.edu>
2019-11-17 17:46:12 -0800

    Added imports for outlier detection

--- train/logreg.py
+++ train/logreg.py
@@ -2,6 +2,8 @@ import readdata
 from text_vectorizer import CV
 from text_vectorizer import TFIDF
 from text_vectorizer import word2vec
+from text_vectorizer import outlierDection
+from OutlierDetectRemove import removeOutliers
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 import sys
@@ -15,7 +17,7 @@ import numpy as np
 def getRemovedVals(X,Y = None,Ftype = "",isTest = False):
 
     X = np.array(X)
-    index,_ = outlierDetection(X,Ftype)
+    index,_ = outlierDection(X,Ftype)
     if not isTest:
         Y = np.array(Y)
         Xrem,Yrem = removeOutliers(index,X,Y,Ftype)
